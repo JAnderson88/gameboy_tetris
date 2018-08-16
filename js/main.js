9d1d8c57542a724ae4803fe93885fe2c7d80204c
janderson88 <jande081@fiu.edu>
2018-08-16 15:02:09 -0400

    Finished first level, moving to next branch

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//selecting elements
+const up = document.getElementById("up_button")
+const right = document.getElementById("right_button")
+const down = document.getElementById("down_button")
+const left = document.getElementById("left_button")
+const b = document.getElementById("b")
+const a = document.getElementById("a")
+const screen = document.getElementById("screen")
+
+
+//define functions
+
+
+
+
+//addEventlisteners and call functions
